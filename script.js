3ad2055ecd763847827bf9b3b3904bf41e2e318c
luksvrd <ljvy9b@gmail.com>
2022-10-25 19:08:47 -0500

    add click event listeners to all buttons

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 // TODO: create variable assignments for the h1 and h2 in the article
 const h1 = document.querySelector("h1");
 const h2 = document.querySelector(".art-title");
+
+// TODO: add click event listeners to all the buttons
+const buttons = document.querySelectorAll("button");
